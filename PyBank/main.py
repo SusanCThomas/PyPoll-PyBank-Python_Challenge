71a72033e3aa4dff947ce2760a01e3275ce08e14
Susan C Thomas <susancthomas@susans-mbp.home>
2020-09-19 03:57:43 -0400

    updated data file

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,22 +6,25 @@ budget_csv = os.path.join("Resources", "budget_data.csv")
 
 month_list = []
 profit_loss_list = []
+total_months = 0
 
 with open(budget_csv, 'r') as csv_file:
     reader = csv.reader(csv_file, delimiter=',')
     next(reader)
     
     for row in reader:
-        month_list.append(str(row[0]))
+        total_months += 1
         profit_loss_list.append(int(row[1]))
-
-total_months = len(month_list)
-#print(total_months)
+print(total_months)
+        
 
 index = 0
 
 for num in profit_loss_list:
     index = index + num
 
-#net_profit = sum(profit_loss_list)
-#print(index)
+#total_profit_loss = sum(profit_loss_list)
+print(index)
+
+
+
