95c5d93f7b143b9a9a2a39831474c56a41dab119
Susan C Thomas <susancthomas@susans-mbp.home>
2020-09-21 03:00:29 -0400

    add data file

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,6 @@ import csv
 #File Path
 budget_csv = os.path.join("Resources", "budget_data.csv")
 
-
 profit_loss_list = []
 months_list = []
 
@@ -12,68 +11,77 @@ months_list = []
 with open(budget_csv, 'r') as csv_file:
     reader = csv.reader(csv_file, delimiter=',')
     next(reader)
-    
+
     for row in reader:
         months_list.append(str(row[0]))
         profit_loss_list.append(int(row[1]))
-#Total Number of Months included in data set
+
+#Total Number of Months included in Data Set
 total_months = len(months_list)
-print(total_months)
-        
+# print(total_months)
 
-#Net Total Amount of "Profit/Losses"
-# index = 0
+#Net Total Amount of "Profits/Losses" over the entire period
+total = 0
 
-# for num in profit_loss_list:
-#     index = index + num
+for num in profit_loss_list:
+    total = total + num
 
-# total_profit_loss = sum(profit_loss_list)
-#print(index)
+# print(total)
 
+#Average of the changes in "Profit/Losses" over the entire period
+average_change = []
+prior_month = 0
 
-#The Average Profit/Loss
-# average_revenue_list = []
-# prior_month = 0
+for x in range(len(profit_loss_list)):
+    if x==0:
+        prior_month = profit_loss_list[x]
+    else:
+        monthly_change = profit_loss_list[x] - prior_month
+        average_change.append(monthly_change)
+        prior_month = profit_loss_list[x]
 
-# for x in range(len(profit_loss_list)):
-#     if x == 0:
-#         prior_month = profit_loss_list[x]
-#     else:
-#         monthly_change = profit_loss_list[x] - prior_month
-#         average_revenue_list.append(monthly_change)
-#         prior_month = profit_loss_list[x]
+# print(average_change)
 
-#print(average_profit_loss_list)
+revenue_average = sum(average_change) / len(average_change)
 
-# revenue_average = sum(average_revenue_list) / len(average_revenue_list)
-#print(revenue_average)
-
-#Calculate the greatest increase in profits (date and amount)
-#Calculate the greatest decrease in losses (date and amount)
-# greatest_increase = 0
-# greatest_increase_month = ''
-# greatest_decrease = 0
-# greatest_decrease_month = ''
+# print(revenue_average)
+y = round(revenue_average, 2)
+# print(y)
 
 
-# for x in range(len(average_revenue_list)):
-#     if average_revenue_list[x] > greatest_increase:
-#         greatest_increase = average_revenue_list[x]
-#         greastest_increase_month = months_list[x+1]
-#     elif average_revenue_list[x] < greatest_decrease:
-#         greatest_decrease = average_revenue_list[x]
-#         greastest_decrease_month = months_list[x+1]
+#Greatest increase in profits (date and amount) over the entire period
+#Greatest decrease in losses (date and amount) over the entire period
 
-#Print Analysis and create text file
-print('Financial Analysis'),
-print('------------------'),
-print('Total Months:' + str(total_months)
+greatest_increase = 0
+greatest_increase_month = ','
+greatest_decrease = 0
+greatest_decrease_month = ','
 
-# print('Net Total Amount of Profits/Losses:' + str(index)),
-# print('Average Revenue Change:' + str(revenue_average)),
-# print('Greatest Increase in Profit:' + (greastest_increase_month) + "($" + (greatest_increase)),
-# print('Greatest Decrease in Losses:' + (greastest_decrease_month) + "($" + (greatest_decrease))
+for x in range(len(average_change)):
+    if average_change[x] > greatest_increase:
+        greatest_increase = average_change[x]
+        greatest_increase_month = months_list[x+1]
+    elif average_change[x] < greatest_decrease:
+        greatest_decrease = average_change[x]
+        greatest_decrease_month = months_list[x+1]
 
+# print(str(greatest_increase_month) + str(greatest_increase))
+# print(str(greatest_decrease_month) + str(greatest_decrease))
 
-# with open('Analysis/budget.txt, 'w') as file:
-#     f.write()
+#Financial Summary
+
+print('Financial Analysis')
+print('------------------')
+print('Total Months:' + " " + str(total_months))
+print("Total:" + " " + "$" + str(total) + "")
+print('Average Change:' + " " + "$" + str(y)+ "")
+print('Greatest Increase in Profits:' + " " + str(greatest_increase_month) + " " +
+ "($" + str(greatest_increase) + ")")
+print('Greatest Decrease in Profits:' + " " + str(greatest_decrease_month) + " " +
+ "($" + str(greatest_decrease) + ")")
+
+#Export Text File 
+with open('analysis/budget.txt', 'w') as f:
+     f.write("Financial Analysis\n")
+     f.write("------------------\n")
+     f.write("Total Months")
