fb605128f43243a2ffaea5421b34b699df4365bf
SusanCThomas <sthomas.a4h@gmail.com>
2021-02-06 03:33:40 -0500

    updated code on main file to print the election results

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,30 +1,58 @@
+# Imprting Dependencies
 import os
 import csv
 
 #File Path
 election_csv = os.path.join("Resources", "election_data.csv")
 
-voter_counts = []
-candidate_list = []
+Voter_ID = []
+Candidate_List = []
+Votes = {}
+number_of_votes = 0
 
 #Read the CSV file
 with open(election_csv, 'r') as csv_file:
     reader = csv.reader(csv_file, delimiter=',')
-    next(reader)
+    csv_header = next(csv_file)
 
+# Conditions
     for row in reader:
-        voter_counts.append(str(row[0]))
+        Voter_ID.append(row[0])
+        Candidate = row[2]
+        if Candidate not in Candidate_List:
+            Candidate_List.append(Candidate)
+            Votes[Candidate] = 1
+        else:
+            Votes[Candidate] += 1
+
+    Total_Candidates = len(Candidate_List)
+    Total_Voters = len(Voter_ID)
+    Most_Votes = sorted(Votes)
         
 
-#Total Number of Votes Cast
-total_votes = len(voter_counts)
-print(total_votes)
+# Election Result Summary
 
-#Complete_list of candidates who received votes
+print('Election Results')
+print('------------------------')
+print('Total Votes:' + str(Total_Voters))
+print('------------------------')
+for Runner in Candidate_List:
+    print(f"{Runner}: {round(Votes[Runner] * 100 / (Total_Voters), 3)}% ({Votes[Runner]})")
+print('------------------------')
+print('Winner:' + str(Most_Votes[1]))
+print('------------------------')
 
+# Export Text File 
+pypoll_analysis = os.path.join("analysis", "election_results.txt")
 
-
-
-#Percentage of votes each candidate won
-#Total number of votes each candidate won
-#Winner of the election based on popular vote.
+with open(pypoll_analysis, 'w') as txt_file:
+    txt_file.write("Election Results\n")
+    txt_file.write("------------------------------\n")
+    txt_file.write('Total Votes:' + str(Total_Voters) + "\n")
+    txt_file.write("------------------------------\n")
+    for Runner in Candidate_List:
+        txt_file.write(f"{Runner}: {round(Votes[Runner] * 100 / (Total_Voters), 3)}% ({Votes[Runner]})" + "\n")
+    
+    txt_file.write("------------------------------\n")
+    txt_file.write(('Winner:' + str(Most_Votes[1])) + "\n")
+    txt_file.write("------------------------------\n")
